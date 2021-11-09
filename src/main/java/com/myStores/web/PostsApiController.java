dd7db5439b00ed021a3d96bd2e01ad6baf630180
byeongSoo <ubs4939@naver.com>
2021-11-10 02:35:16 +0900

    api controller 생성

---
+++ src/main/java/com/myStores/web/PostsApiController.java
@@ -0,0 +1,20 @@
+package com.myStores.web;
+
+import com.myStores.service.PostsService;
+import com.myStores.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requestsDto){
+        return postsService.save(requestsDto);
+    }
+}
