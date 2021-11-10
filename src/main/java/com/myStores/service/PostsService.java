44a464e0b88ec3a843af896c0878ae171d462211
byeongsoo <ubs4939@naver.com>
2021-11-10 14:26:30 +0900

    sevice에 update findById 생성

--- src/main/java/com/myStores/service/PostsService.java
+++ src/main/java/com/myStores/service/PostsService.java
@@ -1,16 +1,36 @@
 package com.myStores.service;
 
+import com.myStores.domain.posts.Posts;
 import com.myStores.domain.posts.PostsRepository;
+import com.myStores.web.dto.PostsResponseDto;
 import com.myStores.web.dto.PostsSaveRequestDto;
+import com.myStores.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class PostsService {
     private final PostsRepository postsRepository;
 
+    @Transactional
     public Long save(PostsSaveRequestDto requestsDto){
         return postsRepository.save(requestsDto.toEntity()).getId();
     }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+        posts.update(requestDto.getTitile(),requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
+
+        return  new PostsResponseDto(entity);
+    }
+
 }
