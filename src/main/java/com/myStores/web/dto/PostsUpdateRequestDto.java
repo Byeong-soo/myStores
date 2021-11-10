8c769385964e46e612a86ff7b9766c13f3f0b804
byeongsoo <ubs4939@naver.com>
2021-11-10 14:26:43 +0900

    PostsUpdateRequestDto 생성

---
+++ src/main/java/com/myStores/web/dto/PostsUpdateRequestDto.java
@@ -0,0 +1,18 @@
+package com.myStores.web.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsUpdateRequestDto {
+    private String titile;
+    private String content;
+
+    @Builder
+    public PostsUpdateRequestDto(String titile, String content){
+        this.titile = titile;
+        this.content = content;
+    }
+}
