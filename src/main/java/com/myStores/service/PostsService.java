f3578234d2798d59edcf9c5ca75d458d26e0c9b4
byeongSoo <ubs4939@naver.com>
2021-11-11 13:32:05 +0900

    findAllDesc 매소드 추가

--- src/main/java/com/myStores/service/PostsService.java
+++ src/main/java/com/myStores/service/PostsService.java
@@ -2,6 +2,7 @@ package com.myStores.service;
 
 import com.myStores.domain.posts.Posts;
 import com.myStores.domain.posts.PostsRepository;
+import com.myStores.web.dto.PostsListResponseDto;
 import com.myStores.web.dto.PostsResponseDto;
 import com.myStores.web.dto.PostsSaveRequestDto;
 import com.myStores.web.dto.PostsUpdateRequestDto;
@@ -9,6 +10,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -31,6 +34,13 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
 
         return  new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 
 }
