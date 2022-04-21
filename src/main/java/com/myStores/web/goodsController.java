0ab6dcf10429f4f998761086ecd097d19f773189
byeongSoo <ubs4939@naver.com>
2022-04-21 19:10:37 +0900

    상품 컨트롤러 생성 - 조회 페이지 form 연결

---
+++ src/main/java/com/myStores/web/goodsController.java
@@ -0,0 +1,17 @@
+package com.myStores.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Slf4j
+@Controller
+@RequestMapping("/goods")
+public class goodsController {
+
+    @GetMapping("/search")
+    public String searchForm(){
+        return "form/goods/goodSearch";
+    }
+}
