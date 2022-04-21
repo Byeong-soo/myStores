bb2494e5c477ae9feea6148fba2b2c3c358d72a9
byeongSoo <ubs4939@naver.com>
2022-04-21 19:12:05 +0900

    index 위치 변경

--- src/main/java/com/myStores/web/IndexController.java
+++ src/main/java/com/myStores/web/IndexController.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
@@ -24,7 +25,8 @@ public class IndexController {
         if(user != null){
             model.addAttribute("loginName",user.getName());
         }
-        return "index";
+
+        return "/form/index";
     }
 
     @GetMapping("/posts/save")
