abb8abe47c760113cf56303b555d10175d6d1b49
ubs <ubs4939@naver.com>
2022-06-04 16:51:02 +0900

    필요없는 맵핑 삭제

--- src/main/java/com/myStores/web/controller/IndexController.java
+++ src/main/java/com/myStores/web/controller/IndexController.java
@@ -1,40 +1,27 @@
 package com.myStores.web.controller;
 
-import com.myStores.config.auth.LoginUser;
-import com.myStores.config.auth.dto.SessionUser;
-import com.myStores.service.PostsService;
-import com.myStores.web.dto.PostsResponseDto;
+import com.myStores.service.GoldPriceService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
-    private final PostsService postsService;
+
+    private final GoldPriceService goldPriceService;
 
     @GetMapping("/")
-    public String index(Model model){ //@LoginUser SessionUser user
+    public String index(Model model){//@LoginUser SessionUser user
 //        model.addAttribute("posts",postsService.findAllDesc());
 //        if(user != null){
 //            model.addAttribute("loginName",user.getName());
 //        }
+
         return "form/index";
     }
 
-    @GetMapping("/posts/save")
-    public String postsSave(){
-        return "posts-save";
-    }
-
-    @GetMapping("/posts/update/{id}")
-    public String postsUpdate(@PathVariable Long id,Model model){
-        PostsResponseDto dto = postsService.findById(id);
-        model.addAttribute("post",dto);
-        return "posts-update";
-    }
 }
