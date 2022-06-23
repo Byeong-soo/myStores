d201521f8c6c8eea04b3c1a6cab3263922a6174b
ubs <ubs4939@naver.com>
2022-06-23 18:06:18 +0900

    item entity -> searchDto 메소드 추가

--- src/main/java/com/myStores/web/dto/SearchItemDto.java
+++ src/main/java/com/myStores/web/dto/SearchItemDto.java
@@ -1,5 +1,6 @@
 package com.myStores.web.dto;
 
+import com.myStores.domain.item.Item;
 import lombok.Getter;
 
 @Getter
@@ -18,15 +19,28 @@ public class SearchItemDto {
 
     private int sum;
 
-    public SearchItemDto(Long id, String modelNumber, Double basicMount, int margin, String memo, int basicWage, int mainWage, int supportWage, int sum) {
-        this.id = id;
-        this.modelNumber = modelNumber;
-        this.basicMount = basicMount;
-        this.margin = margin;
-        this.memo = memo;
-        this.basicWage = basicWage;
-        this.mainWage = mainWage;
-        this.supportWage = supportWage;
-        this.sum = sum;
+//    public SearchItemDto(Long id, String modelNumber, Double basicMount, int margin, String memo, int basicWage, int mainWage, int supportWage, int sum) {
+//        this.id = id;
+//        this.modelNumber = modelNumber;
+//        this.basicMount = basicMount;
+//        this.margin = margin;
+//        this.memo = memo;
+//        this.basicWage = basicWage;
+//        this.mainWage = mainWage;
+//        this.supportWage = supportWage;
+//        this.sum = sum;
+//    }
+
+    public SearchItemDto(Item item){
+        this.id = item.getId();
+        this.modelNumber = item.getModelNumber();
+        this.basicMount = item.getBasicMount();
+        this.margin = item.getMargin();
+        this.memo = item.getMemo();
+        this.basicWage = item.getItemWage().getWagePrice().getBasic();
+        this.mainWage = item.getItemWage().getWagePrice().getMain();
+        this.supportWage = item.getItemWage().getWagePrice().getSupport();
+        this.sum = item.getItemWage().getWagePrice().getSum();
     }
+
 }
